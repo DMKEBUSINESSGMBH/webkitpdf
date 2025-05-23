1bba8df23295dd68c4bb1cd749a2e9208d07971a
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.com>
2025-05-23 11:03:02 +0200

    Cleanup set default value for text fields

--- ext_tables.sql
+++ ext_tables.sql
@@ -5,7 +5,7 @@
 CREATE TABLE tx_webkitpdf_cache (
 	uid int(11) NOT NULL auto_increment,
 	crdate int(11) DEFAULT '0' NOT NULL,
-	urls text NOT NULL,
-	filename tinytext NOT NULL,
+	urls text DEFAULT '' NOT NULL,
+	filename tinytext DEFAULT '' NOT NULL,
 	PRIMARY KEY (uid),
 ) ;
